943762dda3487e3b1b93cde34963a8ecf08e3714
bss8 <borislav.sabotinov@gmail.com>
2020-04-18 20:45:51 -0500

    Add emoji image resources; refactor searchRestUtility to close loading image as needed.

--- public/js/show.js
+++ public/js/show.js
@@ -5,25 +5,25 @@
  */
 class show {
 
-    constructor(key, title, year, description, posterUrl, mainCast, ) {
-        this.key = key;
+    constructor(id, title, url, year, description, posterUrl ) {
+        this.id = id;
         this.title = title;
+        this.url = url;
         this.year = year;
         this.description = description;
         this.posterUrl = posterUrl;
-        this.mainCast = mainCast;
     }
 
     constructEntry() {
         let result = "";
         result += "<div class='card flex-row flex-wrap'>";
         result +=   "<div class='card-header border-0'>";
-        result +=       "<img src='" + this.posterUrl + "' alt=''>";
+        result +=       "<img height='150' width='100' src='" + this.posterUrl + "' alt=''>";
         result +=   "</div>";
         result +=       "<div class='card-block px-2'>";
-        result +=           "<h4 class='card-title'>" + this.title + "</h4>";
+        result +=           "<h4 class='card-title'>" + this.title + " - " + this.year +" </h4>";
         result +=           "<p class='card-text'>" + this.description + "</p>";
-        result +=           "<a href='#' class='btn btn-primary'>BUTTON</a>";
+        result +=           "<a href=" + this.url + " class='btn btn-primary'>Link to Imdb</a>";
         result +=       "</div>";
         result +=       "<div class='w-100'></div>";
         result +=       "<div class='card-footer w-100 text-muted'>";
