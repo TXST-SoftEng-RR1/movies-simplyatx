52a231e58c46bfbc17b0312b754c8452e7d8bb43
bss8 <borislav.sabotinov@gmail.com>
2020-04-19 22:10:51 -0500

    Moved Firebase database connection logic to app.js and removed duplication of config from dbConnectionUtility.js. Added method to update the count for a given title and emoji ID. If the title does not exist in the DB, create it with default values first.
    Signed-off-by: bss8 <borislav.sabotinov@gmail.com>

--- public/js/dbConnectionUtility.js
+++ public/js/dbConnectionUtility.js
@@ -1,35 +1,69 @@
 
-  // Go here for a complete refernece specification for this implementation: 
-  // https://firebase.google.com/docs/reference/js/firebase.database.Reference.html
-  
-const firebaseConfig = {
-  apiKey: "AIzaSyDtt3Yuz864i09jw5bztgfB_1Q4OQ54ZM0",
-  authDomain: "movies-simplyatx.firebaseapp.com",
-  databaseURL: "https://movies-simplyatx.firebaseio.com",
-  projectId: "movies-simplyatx",
-  storageBucket: "movies-simplyatx.appspot.com",
-  messagingSenderId: "357033553685",
-  appId: "1:357033553685:web:6a9768c02f0b907f11831c",
-  measurementId: "G-FHBLZJVSQ2"
-};
-var Database = null;
-  
-function connectDatabase(){
-  firebase.initializeApp(firebaseConfig);
-  // Get a reference to the database service
-  Database = firebase.database();
+// Go here for a complete refernece specification for this implementation:
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference.html
+
+/**
+ * returns a JSON of all emojiIds and their respective counts
+ * @param titeId
+ */
+function getAllEmojiCountsForTitle(titeId){
+	// TODO: given a titleId, query the database and retrive the JSON object if it exists
+	//  otherwise return: empty
 }
 
-function queryDatabase(uid, table, key=null){
-	var dataobject =  Database.ref(table).once('value');
-	if(key){
-		return dataobject[key];
-	}
-	else{
-		return dataobject;
-	}
+/**
+ * returns a numeric type representing the count for a given emojiId
+ *
+ * @param titleId
+ * @param emojiId
+ */
+function getSingleEmojiCountForTitle(titleId, emojiId) {
+	// TODO: query the DB and retrieve the number of reviews for a given emojiId
+	//  if titleId is not in DB, return 0
 }
 
-function updateDatabase(table, key, value) {
-	Database.ref(table).set({key: value)});
+/**
+* https://firebase.google.com/docs/reference/js/firebase.database.Reference#transaction
+* "transaction() is used to modify the existing value to a new value, ensuring there are no conflicts with other
+* clients writing to the same location at the same time."
+*
+* @param titleId
+* @param emojiId
+*/
+function updateSingleEmojiCountForTitle(titleId, emojiId) {
+    // Try to create a record for a titleId (e.g., tt0241527), but only if not already there
+    let titleRef = firebase.database().ref('Reviews/' + titleId);
+    titleRef.transaction(function(currentData) {
+        if (currentData === null) {
+            return { e1: 0, e2: 0, e3: 0, e4: 0, e5: 0, e6: 0, e7: 0, e8: 0, e9: 0 };
+        }
+        else {
+            console.log('Title' + titleId + ' already exists. Updating emojiId only.');
+            return; // Abort the transaction.
+        }
+    }, function(error, committed, snapshot) {
+        if (error) {
+            console.error('Transaction failed abnormally!', error);
+        } else if (!committed) {
+            console.warn('We aborted create transaction. Title already exists.');
+        } else {
+            console.log(titleId + ' created with default 0 values for emojiIds.');
+        }
+    });
+
+    // Increment title's emoji count by 1.
+    var incrementEmojiCount = firebase.database().ref('Reviews/' + titleId + '/' + emojiId);
+    incrementEmojiCount.transaction(function(currentCount) {
+        // If Reviews/title/emoji has never been set, currentCount will be `null`.
+        return currentCount + 1;
+    }, function(error, committed, snapshot) {
+        if (error) {
+            console.error('Transaction failed abnormally!', error);
+        } else if (!committed) {
+            console.warn('We aborted create transaction.');
+        } else {
+            console.log(titleId + ' for emoji ' + emojiId + ' incremented +1!');
+        }
+        console.log(titleId + " data: ", snapshot.val());
+    });
 }
